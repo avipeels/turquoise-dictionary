828b9b583131f05324b4e17ce652113451df1a16
Avinash <avinashpsk@gmail.com>
2021-07-19 16:28:10 +0530

    updated lerna json

--- packages/app-server/server.js
+++ packages/app-server/server.js
@@ -6,7 +6,7 @@ const getValidatedUser = require('./utils');
 
 const init = async () => {
     const server = Hapi.server({
-        port: 3000,
+        port: 5000,
         host: 'localhost'
     });
 
