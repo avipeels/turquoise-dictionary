9c59c0cb9a41cf1c6e83efe3f920dc756ce1a8c9
Avinash <avinashpsk@gmail.com>
2021-07-27 22:03:40 +0530

    validateUser comment added

--- packages/api-sdk/src/login.js
+++ packages/api-sdk/src/login.js
@@ -1,4 +1,4 @@
-// import axios from "axios"
-const axios = require('axios');
 
+const axios = require('axios');
+// validateUser
 export const validateUser = (username, password) => axios.post('http://localhost:5000/login', { username, password });
